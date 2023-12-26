2a09cd8f1b12a2d6a6a10cea2c1811d234aa27c8
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-26 14:12:23 +0900

    231226_the moviedb API 설정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+//themoviedb api
+export const API_URL = "http://api.themoviedb.org/3/";
+
+//image
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
